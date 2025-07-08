52fe87605ab3c822b7d6195de8a37d542f77ce4e
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-08 15:47:38 +0300

    style: add background for root, delete unused css file

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,16 +1,13 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>React App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <h1>React App</h1>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
     </>
   );
