4eabd82267eaa3cff2ac8f312a7f3c4e5209dab3
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-12 10:01:36 +0300

    feat: update Main component with new functionality

--- rs-react-app/src/features/components/Main.tsx
+++ rs-react-app/src/features/components/Main.tsx
@@ -3,8 +3,15 @@ import { Component } from 'react';
 import Search from './Search';
 import ErrorButton from './ErrorButton';
 import Results from './Results';
-
 class Main extends Component {
+  state = {
+    searchQuery: localStorage.getItem('searchQuery') || '',
+  };
+
+  handleSearch = (query: string) => {
+    this.setState({ searchQuery: query });
+  };
+
   render() {
     return (
       <div
@@ -12,14 +19,15 @@ class Main extends Component {
           'bg-dark/4',
           'shadow-3xl/20',
           'flex h-auto flex-col items-center justify-center gap-8',
-          'rounded-[30px]',
-          'p-6',
+          'rounded-[10px]',
+          'm-10 mx-auto p-6',
           'backdrop-invert backdrop-opacity-5',
-          'max-sm:w-sm sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl'
+          'max-w-[1550px]'
+          // 'max-sm:w-sm sm:w-xl md:w-2xl lg:w-4xl xl:w-6xl'
         )}
       >
-        <Search />
-        <Results />
+        <Search onSearch={this.handleSearch} />
+        <Results searchQuery={this.state.searchQuery} />
         <ErrorButton />
       </div>
     );
