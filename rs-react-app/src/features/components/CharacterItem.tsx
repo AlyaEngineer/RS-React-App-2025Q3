764117bcc8da4aac02d318b8a480e7b41965f203
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-12 09:58:43 +0300

    feat: create CharacterItem component to display character details in the card

---
+++ rs-react-app/src/features/components/CharacterItem.tsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import { Character } from '../types';
+
+interface CharacterItemProps {
+  character: Character;
+}
+
+class CharacterItem extends Component<CharacterItemProps> {
+  render() {
+    const { name, species, gender, location, image } = this.props.character;
+
+    return (
+      <div className="flex h-full items-center rounded-md bg-slate-700/80 p-4">
+        <img src={image} alt={name} className="h-25 w-25 rounded-md object-cover" />
+        <div className="ml-4 flex flex-col">
+          <h4 className="text-lg font-semibold text-white">{name}</h4>
+          <p className="text-left text-sm font-medium text-gray-400">race: {species}</p>
+          <p className="text-left text-sm font-medium text-gray-400">gender: {gender}</p>
+          <p className="text-left text-sm font-medium text-gray-400">location: {location.name}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CharacterItem;
