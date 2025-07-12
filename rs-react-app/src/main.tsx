64366711f5f54eb9bc9731984aa4c23e66bfeb5f
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-12 09:44:35 +0300

    feat: implement ErrorBoundary

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style/style.css';
 import App from './App.tsx';
-import ErrorBoundary from './features/ErrorBoundary.tsx';
-import Fallback from './features/Fallback.tsx';
+import ErrorBoundary from './features/components/ErrorBoundary.tsx';
+import Fallback from './features/components/Fallback.tsx';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
