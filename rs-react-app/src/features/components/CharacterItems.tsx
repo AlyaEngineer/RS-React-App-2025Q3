b3e29e6ba32cbbd52c07d811d588767b1877ec3c
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-12 09:59:36 +0300

    feat: add CharacterItems component to render a grid of character cards

---
+++ rs-react-app/src/features/components/CharacterItems.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+import CharacterItem from './CharacterItem';
+import { Character } from '../types';
+
+interface CharacterItemsProps {
+  characters: Character[];
+}
+
+class CharacterItems extends Component<CharacterItemsProps> {
+  render() {
+    const { characters } = this.props;
+    return (
+      <ul className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {characters.map((char: Character) => (
+          <li key={char.id}>
+            <CharacterItem character={char} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default CharacterItems;
