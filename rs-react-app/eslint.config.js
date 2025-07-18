83b3dc4fe541eb98ffb50a47eb6e6c6ace3ad254
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-18 15:11:23 +0300

    chore: install vitest, install testing libraries, add configs

--- rs-react-app/eslint.config.js
+++ rs-react-app/eslint.config.js
@@ -1,39 +1,49 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import reactCompiler from 'eslint-plugin-react-compiler';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import testingLibrary from 'eslint-plugin-testing-library';
+import vitest from '@vitest/eslint-plugin';
 import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.strict,
-      eslintPluginPrettier,
       importPlugin.flatConfigs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      reactHooks.configs['recommended-latest'],
+      jsxA11y.flatConfigs.recommended,
+      eslintConfigPrettier,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      'react-compiler': reactCompiler,
+      'testing-library': testingLibrary,
+      vitest: vitest,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'react-compiler/react-compiler': 'error',
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      '@typescript-eslint/consistent-type-exports': 'off',
+      '@typescript-eslint/consistent-type-imports': 'off',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/consistent-type-definitions': 'off',
       'import/order': [
         'error',
         {
@@ -51,6 +61,13 @@ export default tseslint.config(
         },
       ],
     },
+  },
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    ...testingLibrary.configs['flat/react'],
+    ...vitest.configs.recommended,
+  },
+  {
     settings: {
       react: {
         version: 'detect',
@@ -59,5 +76,5 @@ export default tseslint.config(
         typescript: true,
       },
     },
-  }
+  },
 );
