a22a65756f475dddc8dd626c5429229d996ce5f4
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-21 01:53:16 +0300

    refactor: combine components App and Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Component } from 'react';
 
-import Main from './features/components/Main';
+import ErrorButton from './features/components/errorHandling/ErrorButton';
+import Results from './features/components/fetcher/Results';
+import Search from './features/components/search/Search';
 import { cn } from './libs/utils';
 
 class App extends Component {
+  state = {
+    searchQuery: localStorage.getItem('searchQuery') || '',
+  };
+
+  handleSearch = (query: string) => {
+    this.setState({ searchQuery: query });
+    localStorage.setItem('searchQuery', query);
+  };
+
   render() {
     return (
       <div
@@ -12,7 +23,19 @@ class App extends Component {
           '@container'
         )}
       >
-        <Main />
+        <div
+          className={cn(
+            'bg-dark/4 shadow-3xl/20',
+            'flex h-auto flex-col items-center justify-center gap-8',
+            '@9xl:m-auto rounded-xl p-6',
+            'backdrop-invert backdrop-opacity-5',
+            'max-w-(--my-max-width)'
+          )}
+        >
+          <Search onSearch={this.handleSearch} />
+          <Results searchQuery={this.state.searchQuery} />
+          <ErrorButton />
+        </div>
       </div>
     );
   }
