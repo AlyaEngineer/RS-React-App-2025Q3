ccfc5322b803af3fd2f6d9ca0ddb1b2a127bc4a3
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-25 14:13:53 +0300

    refactor: convert Main.tsx from class to functional component

--- src/features/pages/Main.tsx
+++ src/features/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { cn } from '@/libs/utils';
 
@@ -6,34 +6,31 @@ import ErrorButton from '../components/errorHandling/ErrorButton';
 import Results from '../components/fetcher/Results';
 import Search from '../components/search/Search';
 
-class Main extends Component {
-  state = {
-    searchQuery: localStorage.getItem('searchQuery') || '',
+export default function Main() {
+  const [searchQuery, setSearchQuery] = useState(() => {
+    return localStorage.getItem('searchQuery') || '';
+  });
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    localStorage.setItem('searchQuery', query);
   };
 
-  handleSearch = (query: string) => {
-    this.setState({ searchQuery: query });
-  };
-
-  render() {
-    return (
-      <div
-        className={cn(
-          'bg-dark/4',
-          'shadow-3xl/20',
-          'flex h-auto flex-col items-center justify-center gap-8',
-          'rounded-xl',
-          '@9xl:m-auto p-6',
-          'backdrop-invert backdrop-opacity-5',
-          'max-w-(--my-max-width)'
-        )}
-      >
-        <Search onSearch={this.handleSearch} />
-        <Results searchQuery={this.state.searchQuery} />
-        <ErrorButton />
-      </div>
-    );
-  }
+  return (
+    <div
+      className={cn(
+        'bg-dark/4',
+        'shadow-3xl/20',
+        'flex h-auto flex-col items-center justify-center gap-8',
+        'rounded-xl',
+        '@9xl:m-auto p-6',
+        'backdrop-invert backdrop-opacity-5',
+        'max-w-(--my-max-width)'
+      )}
+    >
+      <Search onSearch={handleSearch} />
+      <Results searchQuery={searchQuery} />
+      <ErrorButton />
+    </div>
+  );
 }
-
-export default Main;
