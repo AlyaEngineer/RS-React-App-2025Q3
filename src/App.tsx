0c94ff5541a7cdea06b926fc8df2c0b5524105e5
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-26 10:55:27 +0300

    feat: add router to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router';
+
 import Main from './features/pages/Main';
 import { cn } from './libs/utils';
 
 export default function App() {
     return (
-      <div
+      <BrowserRouter>
+        <div
         className={cn(
           'min-h-screen w-full overflow-x-hidden overflow-y-auto p-10 max-md:p-5',
           '@container'
         )}
       >
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
       </div>
+      </BrowserRouter>
     );
   }
