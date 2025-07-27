31b77b10549dd5471b3b8fa05ca66e7bdc966748
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-07-27 18:21:21 +0300

    feat: add AboutUs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 import Footer from './features/components/footer/Footer';
+import AboutUs from './features/pages/AboutUs/AboutUs';
+import authorData from './features/pages/AboutUs/authorData';
 import Main from './features/pages/Main';
 import NotFound from './features/pages/NotFound';
 import { cn } from './libs/utils';
@@ -16,6 +18,7 @@ export default function App() {
       >
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/about" element={<AboutUs author={authorData[0]} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
