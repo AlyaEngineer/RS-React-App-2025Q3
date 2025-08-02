af4f993e3c9d39eac9dd637081fe04d7bab1c1b5
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-08-02 07:17:09 +0300

    fix(style): improve styles in CharacterDetail component

--- src/features/components/characters/CharacterDetails.tsx
+++ src/features/components/characters/CharacterDetails.tsx
@@ -7,47 +7,47 @@ export default function CharacterDetails({ character, onClose }: CharacterDetail
     character;
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-6">
       <div className="flex justify-end">
         <button
           className="hover:text-button-error text-white hover:cursor-pointer"
           onClick={onClose}
         >
-          <CircleX strokeWidth={1.25} />
+          <CircleX strokeWidth={1.25} size={32} />
         </button>
       </div>
 
       <div className="flex flex-col">
         <img src={image} alt={name} className="mb-4 h-60 w-60 rounded-xl object-cover" />
         <h2 className="mb-2 text-center text-2xl font-bold text-white">{name}</h2>
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           race: <strong>{species}</strong>
         </p>
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           gender: <strong>{gender}</strong>
         </p>
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           status: <strong>{status}</strong>
         </p>
         {type && (
-          <p className="mb-1 text-gray-400">
+          <p className="mb-1 text-white">
             type: <strong>{type}</strong>
           </p>
         )}
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           origin: <strong>{origin.name}</strong>
         </p>
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           location: <strong>{location.name}</strong>
         </p>
-        <p className="mb-1 text-gray-400">
+        <p className="mb-1 text-white">
           created: <strong>{new Date(created).toLocaleDateString()}</strong>
         </p>
 
-        <div className="w-full">
-          <h3 className="my-2 text-center text-lg font-semibold text-white">Episodes</h3>
-          <div className="mt-4 max-h-96 w-full overflow-y-auto">
-            <ul className="max-h-52 list-disc pl-5 text-lg text-gray-400">
+        <div className="flex w-full flex-col">
+          <h3 className="mt-3 text-center text-xl font-semibold text-white">Episodes</h3>
+          <div className="mt-4 w-full">
+            <ul className="max-h-screen list-disc overflow-y-auto pl-5 text-lg text-white">
               {episode.map((episodeUrl) => {
                 const episodeId = episodeUrl.split('/').pop();
                 return (
