9e3d4ac90552f3be2062c8d3fa7100a42ba2f94c
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-08-06 15:59:11 +0300

    refactor(style): change styles, customize checkbox

--- src/features/components/characters/CharacterItemSkeleton.tsx
+++ src/features/components/characters/CharacterItemSkeleton.tsx
@@ -5,12 +5,12 @@ export default function CharacterSkeleton() {
       aria-label="Loading character preview"
       className="bg-item-card/80 flex h-full animate-pulse items-center rounded-md p-4 max-md:p-2.5"
     >
-      <div className="dark:bg-text-content h-24 w-24 flex-shrink-0 rounded-md bg-slate-500/40" />
+      <div className="dark:bg-text-content-2 bg-text-content-2 h-24 w-24 flex-shrink-0 rounded-md" />
       <div className="ml-4 flex w-full flex-col space-y-2">
-        <div className="dark:bg-text-content h-5 w-1/2 rounded bg-slate-500/30" />
-        <div className="dark:bg-text-content h-4 w-1/3 rounded bg-slate-500/30" />
-        <div className="dark:bg-text-content h-4 w-1/4 rounded bg-slate-500/30" />
-        <div className="dark:bg-text-content h-4 w-1/2 rounded bg-slate-500/30" />
+        <div className="dark:bg-text-content-2 bg-text-content-2 h-5 w-1/2 rounded" />
+        <div className="dark:bg-text-content-2 bg-text-content-2 h-4 w-1/3 rounded" />
+        <div className="dark:bg-text-content-2 bg-text-content-2 h-4 w-1/4 rounded" />
+        <div className="dark:bg-text-content-2 bg-text-content-2 h-4 w-1/2 rounded" />
       </div>
     </div>
   );
