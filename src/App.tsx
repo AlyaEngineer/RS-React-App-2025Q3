771351ed4853c5a95d6eb55b76bbcaf02d740b56
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-08-10 15:54:57 +0300

    chore: install TanStack Query DevTools

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Flyout } from './features/components/flyout/Flyout';
@@ -10,27 +12,32 @@ import Main from './features/pages/Main';
 import NotFound from './features/pages/NotFound';
 import { cn } from './libs/utils';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <div
-          className={cn(
-            'flex min-h-screen w-full flex-col justify-between overflow-x-hidden overflow-y-auto p-10 max-md:p-5 max-sm:px-2',
-            '@container'
-          )}
-        >
-          <Routes>
-            <Route path="/" element={<Main />}>
-              <Route path=":page/:detailsId?" element={<CharacterDetailsPage />} />
-            </Route>
-            <Route path="/about" element={<AboutUs author={authorData[0]} />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Flyout />
-          <Footer />
-        </div>
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <div
+            className={cn(
+              'flex min-h-screen w-full flex-col justify-between overflow-x-hidden overflow-y-auto p-10 max-md:p-5 max-sm:px-2',
+              '@container'
+            )}
+          >
+            <Routes>
+              <Route path="/" element={<Main />}>
+                <Route path=":page/:detailsId?" element={<CharacterDetailsPage />} />
+              </Route>
+              <Route path="/about" element={<AboutUs author={authorData[0]} />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Flyout />
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
