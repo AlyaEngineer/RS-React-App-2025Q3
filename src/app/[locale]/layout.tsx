c5b77b23680d3f691c30b18dfeed8aef20e48872
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-08-20 00:56:17 +0200

    fix: fix layout from locale folder

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,11 +15,5 @@ export default async function LocaleLayout({
     notFound();
   }
 
-  return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
-      </body>
-    </html>
-  );
+  return <NextIntlClientProvider>{children}</NextIntlClientProvider>;
 }
