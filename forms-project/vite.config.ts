065367753db08c2d801bafcab863809e2f3b4339
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-08-23 20:12:58 +0200

    chore: update configs

--- forms-project/vite.config.ts
+++ forms-project/vite.config.ts
@@ -1,24 +1,33 @@
-/// <reference types="vitest/config" />
 import path from 'path';
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'vitest-setup.ts',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
     },
   },
-  test: {
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'html'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    environment: 'jsdom',
-    setupFiles: ['./vitest-setup.js'],
   },
 });
